ce04ccd2d2a3482cd05d491729e091fff0c30a90
Sarvarbek <112868538+sarvar1605@users.noreply.github.com>
2022-09-06 19:03:36 +0500

    Update PrintWordsApp.java

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.print("java\nis\nsmth\n");
+        System.out.println("java\nis\nsmth");
     }
 }
